8e40f68347bfdc66599addba7320b1086d9faf5c
Intzaaa <huangyinhaow@outlook.com>
2024-05-12 11:56:50 +0800

    chore: Update tsup.config.ts for browser platform and shims

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,9 +4,12 @@ export default defineConfig({
   entry: ["./src/index.ts", "./src/router/index.ts"],
   format: ["esm"],
   target: "esnext",
+  platform: "browser",
   sourcemap: true,
   clean: true,
   dts: true,
   splitting: true,
   minify: false,
+  shims: true,
+  skipNodeModulesBundle: true,
 });
